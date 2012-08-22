4ed07adf9aa039fd7f9c9a0d43a13f031550d218
Ben Hamill <git-commits@benhamill.com>
2012-08-22 01:06:04 -0500

    Fix API param name and sugar over nested attributes.

--- lib/contextio/email_settings.rb
+++ lib/contextio/email_settings.rb
@@ -1,10 +1,34 @@
 module ContextIO
   class EmailSettings < APIResource
     def self.discover_for(email_address, source_type = 'imap')
-      attrs = ContextIO::API.request(:get, 'discovery', email_address: email_address, source_type: source_type)
+      attrs = ContextIO::API.request(:get, 'discovery', email: email_address, source_type: source_type)
       new(attrs)
     end
 
     attr_reader :email, :found, :type, :documentation, :imap
+
+    def found?
+      !!found
+    end
+
+    def server
+      imap['server']
+    end
+
+    def username
+      imap['username']
+    end
+
+    def port
+      imap['port']
+    end
+
+    def oauth?
+      !!imap['oauth']
+    end
+
+    def uses_ssl?
+      !!imap['use_ssl']
+    end
   end
 end
