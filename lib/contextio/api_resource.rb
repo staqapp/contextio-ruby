1e601bf4ad1bc9c423ed42dd31091787ef283841
Ben Hamill <git-commits@benhamill.com>
2012-08-22 21:12:33 -0500

    AttributeError's a little strong, don't you think?

--- lib/contextio/api_resource.rb
+++ lib/contextio/api_resource.rb
@@ -3,13 +3,7 @@ require 'json'
 module ContextIO
   class APIResource
     def initialize(attributes)
-      puts attributes.inspect
-
       attributes.each do |name, value|
-        unless self.respond_to?("#{name}")
-          raise ArgumentError, "Unknown attribute '#{name}' for #{self.class.name}"
-        end
-
         self.instance_variable_set("@#{name}", value)
       end
     end
