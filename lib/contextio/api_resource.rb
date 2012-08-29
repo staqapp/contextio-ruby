8d10260206fb902fae933eb96db8e1236795657a
Ben Hamill <git-commits@benhamill.com>
2012-08-28 23:17:56 -0500

    Centralize some API functions.

--- lib/contextio/api_resource.rb
+++ lib/contextio/api_resource.rb
@@ -7,5 +7,32 @@ module ContextIO
         self.instance_variable_set("@#{name}", value)
       end
     end
+
+    def url
+      self.class.instance_url(primary_key)
+    end
+
+    module All
+      def all
+        attr_hashes = ContextIO::API.request(:get, url)
+
+        attr_hashes.map do |attr_hash|
+          new(attr_hash)
+        end
+      end
+    end
+
+    module Fetch
+      def fetch(key)
+        result_hash = ContextIO::API.request(:get, instance_url(key))
+        new(result_hash)
+      end
+    end
+
+    module Delete
+      def delete
+        ContextIO::API.request(:delete, url)
+      end
+    end
   end
 end
--- lib/contextio/oauth_provider.rb
+++ lib/contextio/oauth_provider.rb
@@ -1,11 +1,15 @@
 module ContextIO
   class OAuthProvider < APIResource
-    def self.all
-      attr_hashes = ContextIO::API.request(:get, 'oauth_providers')
+    extend ContextIO::APIResource::All
+    extend ContextIO::APIResource::Fetch
+    include ContextIO::APIResource::Delete
 
-      attr_hashes.map do |attr_hash|
-        new(attr_hash)
-      end
+    def self.url
+      'oauth_providers'
+    end
+
+    def self.instance_url(key)
+      "#{url}/#{key}"
     end
 
     def self.create(type, provider_consumer_key, provider_consumer_secret)
@@ -14,15 +18,10 @@ module ContextIO
       return fetch(result_hash['provider_consumer_key'])
     end
 
-    def self.fetch(key)
-      result_hash = ContextIO::API.request(:get, "oauth_providers/#{key}")
-      new(result_hash)
-    end
-
     attr_reader :type, :provider_consumer_key, :provider_consumer_secret
 
-    def delete
-      ContextIO::API.request(:delete, "oauth_providers/#{provider_consumer_key}")
+    def primary_key
+      provider_consumer_key
     end
   end
 end
