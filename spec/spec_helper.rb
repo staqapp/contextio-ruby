d6e80f1e1f3eda121b4aa1122ed590e4e2a4b3c9
Ben Hamill <git-commits@benhamill.com>
2012-09-07 01:11:24 -0500

    More RSpec configuration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run_including :focus
+  config.order = 'rand'
+  config.color = true
 end
 
 FakeWeb.allow_net_connect = false
