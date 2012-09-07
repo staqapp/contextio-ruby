b10414f4187f4a08e74daee468d499310219a9f9
Ben Hamill <git-commits@benhamill.com>
2012-09-07 00:52:23 -0500

    Prep for testing create.

--- spec/unit/contextio/oauth_provider_collection_spec.rb
+++ spec/unit/contextio/oauth_provider_collection_spec.rb
@@ -4,11 +4,14 @@ require 'contextio/oauth_provider_collection'
 describe ContextIO::OAuthProviderCollection do
   let(:api) { double('API') }
 
-  describe ".new" do
-    subject { ContextIO::OAuthProviderCollection.new(api) }
+  subject { ContextIO::OAuthProviderCollection.new(api) }
 
+  describe ".new" do
     it "takes an api handle" do
       expect(subject.api).to eq(api)
     end
+  end
+
+  describe "#create" do
   end
 end
