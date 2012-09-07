67438b862e83bbe4b23a5f0aecb015be7f401058
Ben Hamill <git-commits@benhamill.com>
2012-09-06 23:12:07 -0500

    Default attributes to empty hash.

--- lib/contextio/api_resource.rb
+++ lib/contextio/api_resource.rb
@@ -3,7 +3,7 @@
 # methods. See below.
 module ContextIO
   class APIResource
-    def initialize(attributes)
+    def initialize(attributes = {})
       attributes.each do |name, value|
         self.instance_variable_set("@#{name}", value)
       end
