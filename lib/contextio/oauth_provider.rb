142966fedd7358e102b9071bb51f478365127fa8
Ben Hamill <git-commits@benhamill.com>
2012-09-08 01:01:20 -0500

    Beef up initialize.

--- lib/contextio/oauth_provider.rb
+++ lib/contextio/oauth_provider.rb
@@ -3,6 +3,12 @@ class ContextIO
     attr_reader :api, :provider_consumer_key
 
     def initialize(api, options = {})
+      required_options = [:provider_consumer_key, 'provider_consumer_key', :resource_url, 'resource_url']
+
+      if (options.keys & required_options).empty?
+        raise ArgumentError, "Either a provider_consumer_key or a resource_url is required"
+      end
+
       @api = api
 
       options.each do |key, value|
--- spec/unit/contextio/oauth_provider_spec.rb
+++ spec/unit/contextio/oauth_provider_spec.rb
@@ -5,15 +5,33 @@ describe ContextIO::OAuthProvider do
   let(:api) { double('API') }
 
   describe ".new" do
-    subject { ContextIO::OAuthProvider.new(api, foo: 'bar', baz: 'bot') }
+    subject { ContextIO::OAuthProvider.new(api, resource_url: 'bar', baz: 'bot') }
 
     it "takes an api handle" do
       expect(subject.api).to eq(api)
     end
 
     it "assigns instance variables for hash arguments" do
-      expect(subject.instance_variable_get('@foo')).to eq('bar')
+      expect(subject.instance_variable_get('@resource_url')).to eq('bar')
       expect(subject.instance_variable_get('@baz')).to eq('bot')
+    end
+
+    context "with a provider_consumer_key passed in" do
+      it "doesn't raise an error" do
+        expect { ContextIO::OAuthProvider.new(api, provider_consumer_key: 'foo') }.to_not raise_error
+      end
+    end
+
+    context "with a resource_url passed in" do
+      it "doesn't raise an error" do
+        expect { ContextIO::OAuthProvider.new(api, resource_url: 'foo') }.to_not raise_error
+      end
+    end
+
+    context "with neither a provider_consumer_key nor a resource_url passed in" do
+      it "raises an ArgumentError" do
+        expect { ContextIO::OAuthProvider.new(api, foo: 'bar') }.to raise_error(ArgumentError)
+      end
     end
   end
 
