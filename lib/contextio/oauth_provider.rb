3a706ad09134e77039e36baef1a128a144e5ec47
Ben Hamill <git-commits@benhamill.com>
2012-09-08 00:48:53 -0500

    OAuthProvider handle its own URL.

--- lib/contextio/oauth_provider.rb
+++ lib/contextio/oauth_provider.rb
@@ -1,6 +1,6 @@
 class ContextIO
   class OAuthProvider
-    attr_reader :api
+    attr_reader :api, :provider_consumer_key
 
     def initialize(api, options = {})
       @api = api
@@ -8,6 +8,16 @@ class ContextIO
       options.each do |key, value|
         instance_variable_set("@#{key}", value)
       end
+    end
+
+    def resource_url
+      @resource_url ||= build_resource_url
+    end
+
+    private
+
+    def build_resource_url
+      "oauth_providers/#{provider_consumer_key}"
     end
   end
 end
--- spec/unit/contextio/oauth_provider_spec.rb
+++ spec/unit/contextio/oauth_provider_spec.rb
@@ -16,4 +16,22 @@ describe ContextIO::OAuthProvider do
       expect(subject.instance_variable_get('@baz')).to eq('bot')
     end
   end
+
+  describe "#resource_url" do
+    context "when input at creation" do
+      subject { ContextIO::OAuthProvider.new(api, resource_url: 'http://example.com') }
+
+      it "uses the input URL" do
+        expect(subject.resource_url).to eq('http://example.com')
+      end
+    end
+
+    context "when not input at creation" do
+      subject { ContextIO::OAuthProvider.new(api, provider_consumer_key: '1234') }
+
+      it "builds its own URL" do
+        expect(subject.resource_url).to eq('oauth_providers/1234')
+      end
+    end
+  end
 end
