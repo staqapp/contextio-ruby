09af2ed670b2bf37ed48c2990ddf7a516e65e1a6
Ben Hamill <git-commits@benhamill.com>
2012-09-09 21:30:29 -0500

    Handle some lazy attributes. Will DRY this up in a bit.

--- lib/contextio/oauth_provider.rb
+++ lib/contextio/oauth_provider.rb
@@ -1,6 +1,6 @@
 class ContextIO
   class OAuthProvider
-    attr_reader :api, :provider_consumer_key
+    attr_reader :api
 
     def initialize(api, options = {})
       required_options = [:provider_consumer_key, 'provider_consumer_key', :resource_url, 'resource_url']
@@ -20,10 +20,42 @@ class ContextIO
       @resource_url ||= build_resource_url
     end
 
+    def type
+      return @type if defined?(@type)
+
+      fetch_attributes
+
+      @type
+    end
+
+    def provider_consumer_key
+      return @provider_consumer_key if defined?(@provider_consumer_key)
+
+      fetch_attributes
+
+      @provider_consumer_key
+    end
+
     private
 
     def build_resource_url
       "oauth_providers/#{provider_consumer_key}"
+    end
+
+    def fetch_attributes
+      attr_hashes = api.request(:get, resource_url)
+
+      attr_hashes.each do |key, value|
+        instance_variable_set("@#{key}", value)
+
+        unless respond_to?(key)
+          instance_eval <<-RUBY
+            def #{key}
+              @#{key}
+            end
+          RUBY
+        end
+      end
     end
   end
 end
--- spec/unit/contextio/oauth_provider_spec.rb
+++ spec/unit/contextio/oauth_provider_spec.rb
@@ -52,4 +52,66 @@ describe ContextIO::OAuthProvider do
       end
     end
   end
+
+  describe "#provider_consumer_key" do
+    context "when input at creation" do
+      subject { ContextIO::OAuthProvider.new(api, provider_consumer_key: '1234') }
+
+      it "uses the input key" do
+        api.should_not_receive(:request)
+
+        expect(subject.provider_consumer_key).to eq('1234')
+      end
+    end
+
+    context "when not input at creation" do
+      subject { ContextIO::OAuthProvider.new(api, resource_url: 'http://example.com/hitme') }
+
+      it "loads it from the API" do
+        api.should_receive(:request).with(
+          :get,
+          'http://example.com/hitme'
+        ).and_return({
+          'provider_consumer_key' => 'baphoo'
+        })
+
+        expect(subject.provider_consumer_key).to eq('baphoo')
+      end
+    end
+  end
+
+  describe "#type" do
+    context "when input at creation" do
+      subject { ContextIO::OAuthProvider.new(api, provider_consumer_key: '1234', type: 'GMAIL') }
+
+      it "uses the input type" do
+        api.should_not_receive(:request)
+
+        expect(subject.type).to eq('GMAIL')
+      end
+    end
+
+    context "when not input at creation" do
+      subject { ContextIO::OAuthProvider.new(api, provider_consumer_key: '1234') }
+
+      it "loads it from the API" do
+        api.should_receive(:request).with(:get, anything).and_return({ 'type' => 'GOOGLEAPPSMARKETPLACE' })
+        expect(subject.type).to eq('GOOGLEAPPSMARKETPLACE')
+      end
+    end
+  end
+
+  describe "#fetch_attributes" do
+    subject { ContextIO::OAuthProvider.new(api, provider_consumer_key: '1234') }
+
+    before do
+      api.stub(:request).with(:get, anything).and_return({ 'foo' => 'foo' })
+    end
+
+    it "defines a getter if one doesn't already exist" do
+      subject.send(:fetch_attributes)
+
+      expect(subject.foo).to eq('foo')
+    end
+  end
 end
