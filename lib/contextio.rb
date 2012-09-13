bcd58eca0e836f2464e162c8819b708065bd0ac0
Ben Hamill <git-commits@benhamill.com>
2012-09-13 00:08:43 -0500

    Write come documentation.

--- lib/contextio.rb
+++ lib/contextio.rb
@@ -1,11 +1,30 @@
-# Yeah
+# Provides an object-oriented interface for the Context.IO API.
+#
+# To use it, you have to [sign up for an account](http://context.io).
+#
+# Once you have an account, you can create a new `ContextIO` object and interact
+# with the API using the methods on that object as a starting point.
 class ContextIO
+  # @private
+  # Handle for the `API` instance. For internal use only.
   attr_reader :api
 
+  # Creates a new `ContextIO` instance and makes a new handle for the API.
+  # This is your entry point to your Context.IO account.  For a web app, you
+  # probably want to instantiate this in some kind of initializer and keep it
+  # around for the life of the process.
+  #
+  # @param [String] key Your OAuth consumer key for your Context.IO account
+  # @param [String] secret Your OAuth consumer secret for your Context.IO
+  #   account
   def initialize(key, secret)
     @api = API.new(key, secret)
   end
 
+  # Your entry point for dealing with oauth providers.
+  #
+  # @return [OAuthProviderCollection] Allows you to work with the providers for
+  #   your account as a group.
   def oauth_providers
     OAuthProviderCollection.new(api)
   end
