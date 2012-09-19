68a9b9d3e4343564b5249401fce561c4721b9a27
Ben Hamill <git-commits@benhamill.com>
2012-09-19 00:02:56 -0500

    Spec and convert some of EmailSettings.

--- lib/contextio/email_settings.rb
+++ lib/contextio/email_settings.rb
@@ -1,11 +1,84 @@
+require 'contextio/api/lazy_attributes'
+
 class ContextIO
-  class EmailSettings < APIResource
-    def self.discover_for(email_address, source_type = 'imap')
+  # Represents the IMAP settings for a given email address. The API
+  # documentation refers to this as 'discovery'. This process is hit-and-miss.
+  # If noting is found, many attributes are likely to be nil and blank. Check
+  # {EmailSettings#found? EmailSettings#found?}.
+  class EmailSettings
+    extend API::LazyAttributes
+
+    # (see ContextIO#api)
+    attr_reader :api
+
+    # @!attribute [r] found
+    #   @return [Boolean] Whether the settings were able to be fetched. Will
+    #     fetch from the API if necessary.
+    # @!attribute [r] type
+    #   @return [String] The type og the provider (like 'gmail'). Will fetch
+    #     from the API if necessary.
+    # @!attribute [r] documentation
+    #   @return [Array<String>] A list of documentation pages that pertain to
+    #     the provider. Will fetch from the API if necessary.
+    lazy_attributes :found, :type, :documentation, :imap
+    private :imap
+
+    # @!attribute [r] email
+    #   @return [String] The email address associated with these IMAP settings.
+    # @!attribute [r] source_type
+    #   @return [String] The only source type currently supported by the API is
+    #     'IMAP'.
+    attr_reader :email, :source_type
+
+    # (see ContextIO::OAuthProviderCollection#initialize)
+    # @param [String] email The email address to fetch the settings for.
+    # @param [String] source_type The only source type currently supported by
+    #   the API is 'IMAP'.
+    def initialize(api, email, source_type = 'IMAP')
+      @api = api
+      @email = email
+      @source_type = source_type
+    end
+
+    # @!attribute [r] resource_url
+    #   @return [String] The path for discovering email settings.
+    def resource_url
+      'discovery'
+    end
+
+    private
+
+    # Fetches attributes from the API for the email settings.
+    #
+    # Defines getter methods for any attributes that come back and don't
+    # already have them. This way, if the API expands, the gem will still let
+    # users get attributes we didn't explicitly declare as lazy.
+    def fetch_attributes
+      attr_hashes = api.request(:get, resource_url, 'email' => email, 'source_type' => source_type)
+
+      attr_hashes.each do |key, value|
+        instance_variable_set("@#{key}", value)
+
+        unless respond_to?(key)
+          instance_eval <<-RUBY
+            def #{key}
+              @#{key}
+            end
+          RUBY
+        end
+      end
+    end
+
+
+
+
+
+
+
+    def self.discover_for(email_address, source_type = 'IMAP')
       attrs = ContextIO::API.request(:get, 'discovery', email: email_address, source_type: source_type)
       new(attrs)
     end
-
-    attr_reader :email, :found, :type, :documentation, :imap
 
     def found?
       !!found
---
+++ spec/unit/contextio/email_settings_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+require 'contextio/email_settings'
+
+describe ContextIO::EmailSettings do
+  let(:api) { double('API') }
+
+  subject { ContextIO::EmailSettings.new(api, 'email@email.com') }
+
+  describe ".new" do
+    context "without a source type passed in" do
+      it "takes an api handle" do
+        expect(subject.api).to eq(api)
+      end
+
+      it "takes an email address" do
+        expect(subject.email).to eq('email@email.com')
+      end
+
+      it "defaults the source type to 'IMAP'" do
+        expect(subject.source_type).to eq('IMAP')
+      end
+    end
+
+    context "with a source type passed in" do
+      subject { ContextIO::EmailSettings.new(api, 'email@email.com', 'FOO') }
+
+      it "takes a source type argument" do
+        expect(subject.source_type).to eq('FOO')
+      end
+    end
+  end
+
+  describe "#resource_url" do
+    it "builds the right url" do
+      expect(subject.resource_url).to eq('discovery')
+    end
+  end
+
+  describe "#documentation" do
+    it "fetches it from the api" do
+      api.should_receive(:request).with(:get, anything, anything).and_return({ 'documentation' => ['foo', 'bar'] })
+      expect(subject.documentation).to eq(['foo', 'bar'])
+    end
+  end
+
+  describe "#found" do
+    it "fetches it from the api" do
+      api.should_receive(:request).with(:get, anything, anything).and_return({ 'found' => true })
+      expect(subject.found).to eq(true)
+    end
+  end
+
+  describe "#type" do
+    it "fetches it from the api" do
+      api.should_receive(:request).with(:get, anything, anything).and_return({ 'type' => 'gmail' })
+      expect(subject.type).to eq('gmail')
+    end
+  end
+
+  describe "#fetch_attributes" do
+    before do
+      api.stub(:request).with(:get, anything, anything).and_return({ 'foo' => 'bar' })
+    end
+
+    it "defines a getter if one doesn't already exist" do
+      subject.send(:fetch_attributes)
+
+      expect(subject.foo).to eq('bar')
+    end
+
+    it "hits the right URL" do
+      api.should_receive(:request).with(:get, 'discovery', 'email' => 'email@email.com', 'source_type' => 'IMAP')
+
+      subject.send(:fetch_attributes)
+    end
+  end
+end
