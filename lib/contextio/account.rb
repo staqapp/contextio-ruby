d5d4f17ba34d3b63f27d7670965f1dc15b63ff3e
Ben Hamill <git-commits@benhamill.com>
2012-10-10 23:52:24 -0500

    Account attributes.

--- lib/contextio/account.rb
+++ lib/contextio/account.rb
@@ -6,6 +6,59 @@ class ContextIO
 
     self.primary_key = :id
 
+    # @!attribute [r] id
+    #   @return [String] The id assigned to this account by Context.IO.
+    # @!attribute [r] username
+    #   @return [String] The username assigned to this account by Context.IO.
+    # @!attribute [r] email_addresses
+    #   @return [Array<String] The email addresses associated with this account.
+    # @!attribute [r] first_name
+    #   @return [String] The account holder's first name.
+    # @!attribute [r] last_name
+    #   @return [String] The account holder's last name.
+    lazy_attributes :id, :username, :created, :suspended, :email_addresses,
+                    :first_name, :last_name, :password_expired, :nb_messages,
+                    :nb_files
+    private :created, :suspended, :password_expired
+
+    # @!attribute [r] created_at
+    #   @return [Time] The time this account was created (with Context.IO).
+    def created_at
+      @created_at ||= Time.at(created)
+    end
+
+    # @!attribute [r] suspended_at
+    #   @return [Time] The time this account was suspended.
+    def suspended_at
+      return @suspended_at if instance_variable_defined?(:@suspended_at)
+
+      @suspended_at = suspended == 0 ? nil : Time.at(suspended)
+
+      @suspended_at
+    end
+
+    # @!attribute [r] suspended?
+    #   @return [Boolean] Whether this account is currently suspended.
+    def suspended?
+      !!suspended_at
+    end
+
+    # @!attribute [r] password_expired_at
+    #   @return [Time] The time this account's password expired.
+    def password_expired_at
+      return @password_expired_at if instance_variable_defined?(:@password_expired_at)
+
+      @password_expired_at = password_expired == 0 ? nil : Time.at(password_expired)
+
+      @password_expired_at
+    end
+
+    # @!attribute [r] password_expired?
+    #   @return [Boolean] Whether this account's password is expired.
+    def password_expired?
+      !!password_expired_at
+    end
+
     private
 
     # Builds the path that will fetch the attributes for this account.
@@ -14,24 +67,6 @@ class ContextIO
     def build_resource_url
       "accounts/#{id}"
     end
-
-    # def self.url
-    #   'accounts'
-    # end
-
-    # def self.create(email, options={})
-    #   result_hash = ContextIO::API.request(:post, url, email: email, first_name: options[:first_name], last_name: options[:last_name])
-
-    #   fetch(result_hash['id'])
-    # end
-
-    # attr_reader :id, :username, :created, :suspended, :email_addresses,
-    #             :first_name, :last_name, :password_expired, :sources,
-    #             :nb_messages, :nb_files
-
-    # def primary_key
-    #   id
-    # end
 
     # def update(attributes = {})
     #   attrs = {}
@@ -42,26 +77,6 @@ class ContextIO
     #   return nil if attrs.empty?
 
     #   ContextIO::API.request(:post, url, attrs)['success']
-    # end
-
-    # def created_at
-    #   Time.at(created) if created
-    # end
-
-    # def suspended_at
-    #   Time.at(suspended) if suspended_at
-    # end
-
-    # def suspended?
-    #   !!suspended
-    # end
-
-    # def password_expired_at
-    #   Time.at(password_expired) if password_expired
-    # end
-
-    # def password_expired?
-    #   !!password_expired
     # end
   end
 end
