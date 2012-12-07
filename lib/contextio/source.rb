0025debb141d2256f25ac304607d687514b26a33
Ben Hamill <git-commits@benhamill.com>
2012-12-07 01:09:26 -0600

    Refactor update.

--- lib/contextio/source.rb
+++ lib/contextio/source.rb
@@ -27,35 +27,22 @@ class ContextIO
     #   provider_token_secret, provider_consumer_key. See the Context.IO docs
     #   for more details on these fields.
     def update(options={})
-      status                = options[:status] || options['status']
-      sync_period           = options[:sync_period] || options['sync_period']
-      service_level         = options[:service_level] || options['service_level']
-      password              = options[:password] || options['password']
-      provider_token        = options[:provider_token] || options['provider_token']
-      provider_token_secret = options[:provider_token_secret] || options['provider_token_secret']
-      provider_consumer_key = options[:provider_consumer_key] || options['provider_consumer_key']
+      updatable_attrs = %w(status sync_period service_level password
+                           provider_token provider_token_secret
+                           provider_consumer_key)
 
-      attrs = {}
-      attrs[:status]                = status if status
-      attrs[:sync_period]           = sync_period if sync_period
-      attrs[:service_level]         = service_level if service_level
-      attrs[:password]              = password if password
-      attrs[:provider_token]        = provider_token if provider_token
-      attrs[:provider_token_secret] = provider_token_secret if provider_token_secret
-      attrs[:provider_consumer_key] = provider_consumer_key if provider_consumer_key
+      options.keep_if do |key, value|
+        updatable_attrs.include?(key.to_s)
+      end
 
-      return nil if attrs.empty?
+      return nil if options.empty?
 
-      it_worked = api.request(:post, resource_url, attrs)['success']
+      it_worked = api.request(:post, resource_url, options)['success']
 
       if it_worked
-        @status                = status || @status
-        @sync_period           = sync_period || @sync_period
-        @service_level         = service_level || @service_level
-        @password              = password || @password
-        @provider_token        = provider_token || @provider_token
-        @provider_token_secret = provider_token_secret || @provider_token_secret
-        @provider_consumer_key = provider_consumer_key || @provider_consumer_key
+        options.each do |key, value|
+          instance_variable_set("@#{key}", value)
+        end
       end
 
       it_worked
